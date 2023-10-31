ae30794acb46f9314e9d389659b519fbdf7f9360
ThumulaAkshay <akshaykumartumula@gmail.com>
2023-10-31 20:42:29 +0530

    my first project

---
+++ numpy.py
@@ -0,0 +1,10 @@
+#if an array contains a single floating point the entire elements are converted to floats
+# @ character represents the matrix multiplication in the numpy
+weights
+array ( [0.3, 0.2, 0.5])
+np.matmul(climate_data, weights)
+array([56.8, 76.9, 81.9, 57.7, 74.9])
+climate_data @ weights
+#concatenate functionis used to concatenate the two arrrays
+#use eye function to create the identity function
+#in linspace function the 3rd argument is number of elements that are equally spaced
